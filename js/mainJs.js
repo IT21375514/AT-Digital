22ba804dcdad160a1be1e0e044563d5f8f581ed3
Sanjayan0 <sanjayan201@gmail.com>
2023-12-19 03:06:12 +0530

    Initial Commit

---
+++ js/mainJs.js
@@ -0,0 +1,26 @@
+﻿// Fetch content from the HomeContent.html file
+fetch('HomeContent.html')
+    .then(response => response.text())
+    .then(data => {
+        // Insert the content into the contentContainer
+        document.getElementById('homeContentContainer').innerHTML = data;
+    })
+    .catch(error => console.error('Error fetching content:', error));
+
+
+//Get navigate to top button
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
